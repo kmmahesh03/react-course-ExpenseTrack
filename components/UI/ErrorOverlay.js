fd2537f0fef248ab83f5d0032a2bbe758347c4cc
kmmahesh03 <57348583+kmmahesh03@users.noreply.github.com>
2022-12-08 12:47:49 +0530

    Add files via upload

---
+++ components/UI/ErrorOverlay.js
@@ -0,0 +1,42 @@
+import {View,Text,StyleSheet} from 'react-native';
+import { GlobalStyles } from '../../constants/styles';
+import Button from './Button';
+function ErrorOverlay({message,onConfrim})
+{
+    return (
+        <View style={styles.container}>
+           <Text style={[styles.text,styles.title]}>An error occurred!...</Text>
+           <Text style={styles.text}>{message}</Text>
+           <Button onPress={onConfrim}>Okay</Button>
+        </View>
+    );
+}
+export default ErrorOverlay;
+
+const styles=StyleSheet.create({
+    container:
+    {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        padding:24,
+        backgroundColor:GlobalStyles.colors.primary700,
+    },
+    text:
+    {
+        textAlign:'center',
+        marginBottom:8,
+        color:'white',
+    },
+    title:
+    {
+      fontSize:20,
+      fontWeight:'bold',
+
+    },
+    message:
+    {
+        fontSize:14
+    },
+
+});
